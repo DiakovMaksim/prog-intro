19dc3b199b03eafea5f3443348f4fec6e15352b7
Diakov Maksim <maksim2004d@yandex.ru>
2022-10-12 11:43:41 +0300

    Scanner upgraded.

--- java-solutions/Scanner.java
+++ java-solutions/Scanner.java
@@ -1,41 +1,6 @@
 import java.io.*;
 import java.util.Arrays;
 
-/*
-Testing Base
-    Running test 01: java Reverse [1 input lines]
-    Running test 02: java Reverse [2 input lines]
-    Running test 03: java Reverse [3 input lines]
-    Running test 04: java Reverse [4 input lines]
-Exception in thread "main" java.lang.AssertionError: Line 3:
-     expected ``,
-       actual `3 2 1`
-        at base.Asserts.error(Asserts.java:75)
-        at base.Asserts.assertTrue(Asserts.java:41)
-        at base.Asserts.assertEquals(Asserts.java:20)
-        at base.Runner.lambda$testEquals$0(Runner.java:36)
-        at base.TestCounter.lambda$test$0(TestCounter.java:58)
-        at base.TestCounter.lambda$testV$2(TestCounter.java:71)
-        at base.Log.silentScope(Log.java:40)
-        at base.TestCounter.testV(TestCounter.java:70)
-        at base.TestCounter.test(TestCounter.java:57)
-        at base.Runner.testEquals(Runner.java:30)
-        at reverse.ReverseTester$Checker.test(ReverseTester.java:102)
-        at reverse.ReverseTester$Checker.test(ReverseTester.java:96)
-        at reverse.ReverseTester$Checker.test(ReverseTester.java:149)
-        at reverse.ReverseTester.run(ReverseTester.java:67)
-        at reverse.ReverseTester.lambda$variant$2(ReverseTester.java:44)
-        at base.Selector.lambda$test$2(Selector.java:79)
-        at base.Log.lambda$action$0(Log.java:18)
-        at base.Log.silentScope(Log.java:40)
-        at base.Log.scope(Log.java:31)
-        at base.Log.scope(Log.java:24)
-        at base.Selector.lambda$test$3(Selector.java:79)
-        at java.base/java.util.ArrayList.forEach(ArrayList.java:1511)
-        at base.Selector.test(Selector.java:79)
-        at base.Selector.main(Selector.java:51)
-        at reverse.FullFastReverseTest.main(FullFastReverseTest.java:15)
- */
 public class Scanner {
     String inputName;
     String charSet;
@@ -116,16 +81,13 @@ public class Scanner {
         if (position >= quantity & quantity > 0) {
             rebuffering();
         }
-        boolean inNext = false;
         StringBuilder nextLine = new StringBuilder();
         while (position < quantity) {
             if (!(buffer[position] == '\n')) {
-                inNext = true;
                 nextLine.append(buffer[position]);
             } else {
-                //    if (inNext) {
-                    return nextLine.toString();
-                //}
+                position++;
+                return nextLine.toString();
             }
             position++;
             if (position >= quantity & quantity > 0) {
@@ -168,16 +130,10 @@ public class Scanner {
         return nextabc;
     }
     public boolean hasNextLine() {
-        while (quantity > 0) {
-            while (position < quantity) {
-                if (!(buffer[position] == '\n')) {
-                    return true;
-                }
-                position++;
-            }
-            rebuffering();
+        if (position >= quantity & quantity < buffer.length) {
+            return false;
         }
-        return false;
+        return true;
     }
     public boolean hasNextInt() {
         return hasNext();
