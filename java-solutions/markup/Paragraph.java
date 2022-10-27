e4e139798ac70eea4676bbd12d61fe31d30c6e47
Michael Ruzavin <misharuz25@gmail.com>
2022-10-27 17:08:00 +0300

    Checked by an instructor

--- java-solutions/markup/Paragraph.java
+++ java-solutions/markup/Paragraph.java
@@ -5,17 +5,16 @@ import java.util.List;
 public class Paragraph extends MarkUpElement {
     public Paragraph(List<MarkUpElement> entry) {
         super(entry);
+        super.specialSymbolStart = "";
+        super.specialSymbolEnd = "";
     }
 
     @Override
     public void toMarkdown(StringBuilder entry) {
-        super.specialSymbolStart = "";
         super.toMarkdown(entry);
     }
     @Override
     public void toTex(StringBuilder entry) {
-        super.specialSymbolStart = "";
-        super.specialSymbolEnd = "";
         super.toTex(entry);
     }
 }
