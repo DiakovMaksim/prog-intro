7362058c90fb61c278ead32b204de4adc20653d2
Diakov Maksim <maksim2004d@yandex.ru>
2022-12-11 15:34:43 +0300

    Add hw Sum.

--- java-solutions/Sum.java
+++ java-solutions/Sum.java
@@ -1,4 +1,23 @@
 public class Sum {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+		String enter = "";
+		for (String part : args) {
+			enter += part + " ";
+		}
+		int result = 0;
+		int startofnumber = -1;
+		for (int i = 0; i < enter.length(); i++) {
+			if (!(Character.isWhitespace(enter.charAt(i)))) {
+				if (startofnumber == -1) {
+					startofnumber = i;
+				}
+			} else {
+				if (startofnumber != -1) {
+					result += Integer.parseInt(enter.substring(startofnumber, i));
+					startofnumber = -1;
+				}
+			}
+		}
+		System.out.println(result);
     }
 }
